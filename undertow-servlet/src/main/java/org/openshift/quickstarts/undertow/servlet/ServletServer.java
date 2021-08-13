a83420e9a3feb60e174c9de108d9dec82b31e151
bparinas <bparinas@hotmail.com>
2021-08-13 15:58:01 +0800

    Update ServletServer.java

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Hello World")
+                                    .addInitParam("message", "Awesome DevOps ")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
